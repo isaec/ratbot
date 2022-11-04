b5297d338faedec30dcdedda57563ae78d87e705
Isaac <isaac@rubenfamily.com>
2022-11-03 17:01:30 -0700

    get sheet reading working

--- src/gsheetReader.ts
+++ src/gsheetReader.ts
@@ -1,26 +1,58 @@
-import GSheetsReader from "g-sheets-api";
 import * as dotenv from "dotenv";
+import { GoogleSpreadsheet, GoogleSpreadsheetRow } from "google-spreadsheet";
+import * as serviceAccount from "../service_account.json";
 dotenv.config();
 
-const options = {
-  apiKey: process.env.GSHEET_API_KEY,
-  sheetId: process.env.GSHEET_SHEET_ID,
-  returnAllResults: true,
+const doc = new GoogleSpreadsheet(process.env.GSHEET_SHEET_ID);
+await doc.useServiceAccountAuth({
+  client_email: serviceAccount.client_email,
+  private_key: serviceAccount.private_key,
+});
+await doc.loadInfo();
+const sheet = await doc.sheetsByTitle[
+  process.env.GSHEET_SHEET_TITLE ?? "purchase requests"
+];
+
+const headerRowIndex = parseInt(process.env.GSHEET_HEADER_ROW ?? "1");
+
+const headerRow = await sheet.loadHeaderRow(headerRowIndex);
+
+// /^\s+(?:'|\s)([^':]*).*/gm
+type PurchaseRequestKeys =
+  | "Line #"
+  | "Status"
+  | "Request Date"
+  | "Approval Date"
+  | "Ordered Date"
+  | "Requestor Name"
+  | "Item"
+  | "Supplier"
+  | "URL"
+  | "Qty"
+  | "Price Per"
+  | "Total Price (no tax)"
+  | "Total Price Paid (including shipping)"
+  | "Price + Tax"
+  | "PO?"
+  | "Invoice Sent"
+  | "Lead"
+  | "Lead Mentor"
+  | "Category"
+  | "Comments";
+
+type PurchaseRequestData = Partial<Record<PurchaseRequestKeys, string>>;
+
+type PurchaseRequestRow = GoogleSpreadsheetRow & PurchaseRequestData;
+
+const getRow = async (rowNumber: number) => {
+  const rows = await sheet.getRows({
+    offset: rowNumber - (headerRowIndex + 1),
+    limit: 1,
+  });
+  return rows[0] as PurchaseRequestRow;
 };
 
-const reader = GSheetsReader(
-  options,
-  (results) => {
-    console.log(results);
-  },
-  (error) => {
-    console.error(error);
-  }
-);
-
-await reader;
-console.log(reader);
-
 export const readLine = async (line: number) => {
+  const row = await getRow(line);
   return 3;
 };
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 import { accumulate, getAllLines } from "./getAllLines";
 import { readLine } from "./gsheetReader";
 
-readLine(2);
+readLine(100);
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ const app = new App({
 });
 
 app.message(/lines?\s*\d+/, async ({ message, say }) => {
-  const lines = getAllLines(message.text);
+  const lines = accumulate(getAllLines(message.text).values());
   if (lines.length === 0) return;
 
   await say(

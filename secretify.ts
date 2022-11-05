747bfba7626d0a9f3b35de09696581fb4b54e181
Isaac <isaac@rubenfamily.com>
2022-11-04 23:54:46 -0700

    use env value instead of reading json

--- secretify.ts
+++ secretify.ts
@@ -7,10 +7,15 @@ const envPath = ".env";
 
 const upperCasePrefixed = (str: string) => `${prefix}_${str}`.toUpperCase();
 
+const fsFile = await Deno.open(envPath, { create: true, append: true });
+
 for (const [key, value] of Object.entries(fileObj)) {
   console.log({ key, value });
-  const envVar = `${upperCasePrefixed(key)}=${value.replaceAll("\n", "\\n")}\n`;
-  await Deno.writeTextFile(envPath, envVar, {
-    append: true,
-  });
+  const envVar = `${upperCasePrefixed(key)}='${value.replaceAll(
+    `\n`,
+    `\\n`
+  )}'\n`;
+  await Deno.write(fsFile.rid, new TextEncoder().encode(envVar));
 }
+
+fsFile.close();

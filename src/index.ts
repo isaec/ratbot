9bd18ba788936de1dd8637baea6fdd11450c3462
Isaac <isaac@rubenfamily.com>
2022-11-04 23:00:47 -0700

    handle errors

--- src/index.ts
+++ src/index.ts
@@ -21,27 +21,35 @@ app.message(/lines?\s*\d+/, async ({ message, say }) => {
   console.log(`message detected in ${message.channel}`);
   if (message.channel !== process.env.SLACK_PURCHASE_CHANNEL_ID) return;
 
-  const lines = getAllLines(message.text);
-  if (lines.size === 0) return;
+  try {
+    const lines = getAllLines(message.text);
+    if (lines.size === 0) return;
 
-  // fetch line data from gsheet
-  const lineData = await Promise.all(
-    accumulate(lines.values())
-      .sort((a, b) => a - b)
-      .map(readSheetLine)
-  );
+    // fetch line data from gsheet
+    const lineData = await Promise.all(
+      accumulate(lines.values())
+        .sort((a, b) => a - b)
+        .map(readSheetLine)
+    );
 
-  // format the data
-  const formattedData = formatDataArray(lineData);
+    // format the data
+    const formattedData = formatDataArray(lineData);
 
-  // make string version of data
-  const str = formattedDataArrayToString(formattedData);
+    // make string version of data
+    const str = formattedDataArrayToString(formattedData);
 
-  await say({
-    blocks: formattedData,
-    thread_ts: message.ts,
-    text: str,
-  });
+    await say({
+      blocks: formattedData,
+      thread_ts: message.ts,
+      text: str,
+    });
+  } catch (e) {
+    console.error("message triggered an error", { message, e });
+    await say({
+      text: `This message triggered an error. Details have been logged. message.text is "${message.text}" ${e}`,
+      thread_ts: message.ts,
+    });
+  }
 });
 
 await app.start(process.env.PORT || 3000).then(() => {

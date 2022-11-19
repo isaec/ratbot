53bb3a7fc53343e3eb6e8ee0d3cacfb0428adf10
Isaac <isaac@rubenfamily.com>
2022-11-18 20:34:40 -0800

    tweak command interface

--- src/commands/commandInterface.ts
+++ src/commands/commandInterface.ts
@@ -6,9 +6,11 @@ type AppInstance = App<StringIndexed>;
 type Message = KnownEventFromType<"message">;
 
 export const commandEvents = makeEnum({
-  Success: val("Success"),
+  /** Show help message explaining how to use command. Useful for validation failure. */
   Help: val("Help"),
-  Error: val("Error", ""),
+  /** Error that cannot be corrected for, in ratbot code. */
+  Error: val<"Error", any>("Error", ""),
+  /** A message to send in thread. */
   Message: val("Message", ""),
 });
 export type CommandEvents = EnumValues<typeof commandEvents>;
@@ -29,32 +31,35 @@ export const registerCommand = (
 ) => {
   app.message(command.commandRegex, async ({ message, say }) => {
     console.log(`${command.name} command detected in ${message.channel}`);
+
+    const sayThread = async (text: string) => {
+      await say({
+        text,
+        thread_ts: message.ts,
+      });
+    };
+
     const iterator = command.executer(app, message);
     for await (const event of iterator) {
       switch (event.name) {
-        case commandEvents.names.Success:
-          await say({
-            text: `Success!`,
-            thread_ts: message.ts,
-          });
-          break;
         case commandEvents.names.Help:
-          await say({
-            text: command.howToUse,
-            thread_ts: message.ts,
-          });
+          await sayThread(command.howToUse);
           break;
         case commandEvents.names.Error:
-          await say({
-            text: `This message triggered an error. Details have been logged. message.text is "${message.text}" ${event.param}`,
-            thread_ts: message.ts,
-          });
-          break;
+          await sayThread(
+            `This message triggered an error. Details have been logged. message.text is "${message.text}" \n${event.param}`
+          );
+          console.error(
+            `message triggered an error in command ${command.name}`,
+            {
+              message,
+              e: event.param,
+            }
+          );
+          // in the event of an error we should stop execution of the command
+          return;
         case commandEvents.names.Message:
-          await say({
-            text: event.param,
-            thread_ts: message.ts,
-          });
+          await sayThread(event.param);
           break;
         default:
           assertExhaustiveSwitch(event);

b6add1db3f8c08a4eb9d222624db8e81e30e64c9
Isaac <isaac@rubenfamily.com>
2022-11-18 17:54:50 -0800

    continue work on enums

--- src/enum.ts
+++ src/enum.ts
@@ -1,3 +1,33 @@
-const enumValue =
-  <T>(key: string, _type: T) =>
-  (value: T) => ({ [key]: value });
+interface EnumValue<N extends string, T = false> {
+  name: N;
+  param: T;
+}
+
+type ParamEnumValue<N extends string, P> = (param: P) => EnumValue<N, P>;
+
+type NamedEnumValue<N extends string> = EnumValue<N, false>;
+
+const makeEnumValue = <N extends string, P>(
+  name: N,
+  param: P
+): EnumValue<N, P> => ({
+  name,
+  param,
+});
+
+export function val<N extends string>(name: N): NamedEnumValue<N>;
+export function val<N extends string, P>(
+  name: N,
+  param: P
+): ParamEnumValue<N, P>;
+export function val<N extends string, P>(name: N, param?: P) {
+  return param === undefined
+    ? makeEnumValue(name, false)
+    : (param: P) => makeEnumValue(name, param);
+}
+
+const testEnum = {
+  Success: val("Success"),
+  Error: val("Error"),
+  Message: val("Message", ""),
+};

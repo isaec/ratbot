33e07644d8b0c3bd8c208290f4c9d5a457a0e771
Isaac <isaac@rubenfamily.com>
2022-11-21 14:47:39 -0800

    start displaying details of command status

--- src/commands/ratstatusCommand.ts
+++ src/commands/ratstatusCommand.ts
@@ -1,4 +1,6 @@
+import { md, section, SlackBlock } from "@src/helpers/slack/blocks";
 import { commandEvents, CommandInterface } from "./commandInterface";
+import { registeredCommands } from "./injectCommands";
 
 export default {
   name: "ratstatus",
@@ -6,5 +8,21 @@ export default {
   commandRegex: /^\.ratstatus$/i,
   executer: async function* (app, { message, say }) {
     yield commandEvents.Message("querying ratbot status details...");
+    const details: SlackBlock = [];
+    for (const [name, { errorGetter }] of registeredCommands) {
+      const errors = errorGetter();
+      if (errors.length === 0) {
+        details.push(section(md(`command *${name}* is loaded and healthy`)));
+      } else {
+        details.push(
+          section([
+            md(`command *${name}* is loaded but has errors`),
+            md(`errors:`),
+            ...errors.map((e) => md(`${e}`)),
+          ])
+        );
+      }
+    }
+    yield commandEvents.BlockMessage(details);
   },
 } as CommandInterface;
